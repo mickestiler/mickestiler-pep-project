e98a19e8e1ae651e93bc4798540595cf93d21315
Mick Estiler <mickestiler@gmail.com>
2024-10-28 16:39:19 +0000

    changes to MessageDAO

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -9,10 +9,31 @@ import Util.ConnectionUtil;
 
 public class MessageDAO {
     
+    /**
+     * Method to insert a message into the message table.
+     * @param message
+     * @return the inserted message
+     */
     public Message insertMessage(Message message) {
         Connection connection = ConnectionUtil.getConnection();
         try {
-            String sql = "INSERT INTO message VALUES ?, ?, ?, ?";
+            String sql = "INSERT INTO message(posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            int posted_by = message.getPosted_by();
+            String message_text = message.getMessage_text();
+            Long time_posted_epoch = message.getTime_posted_epoch();
+
+            preparedStatement.setInt(1, posted_by);
+            preparedStatement.setString(2, message_text);
+            preparedStatement.setLong(3, time_posted_epoch);
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if (pkeyResultSet.next()) {
+                int generated_message_id = (int) pkeyResultSet.getInt(1);
+                message.setMessage_id(generated_message_id);
+                return message;
+            }
         }
         return null;
     }
@@ -29,7 +50,7 @@ public class MessageDAO {
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
-            while(rs.next()){
+            while (rs.next()){
                 Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"),
                                             rs.getString("message_text"), rs.getLong("time_posted_epoch"));
                 messages.add(message);
@@ -38,5 +59,87 @@ public class MessageDAO {
             System.out.println(e.getMessage());
         }
         return messages;
+    }
+
+    /**
+     * Method to obtain a message from the message database.
+     * @param message_id
+     * @return message
+     */
+    public Message getMessageById(int message_id) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM message WHERE message_id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()){
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"),
+                                            rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                return message;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Method to delete message by id.
+     * @param message_id
+     */
+    public void deleteMessageById(int message_id) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "DELETE FROM message WHERE message_id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, message_id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    
+    /**
+     * Method to update message_text by message_id.
+     * @param message_id
+     */
+    public void updateMessageById(int message_id, String message_text) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "UPDATE message SET message_text = ? WHERE message_id = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, message_text);
+            preparedStatement.setInt(2, message_id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public List<Message> getAllMessagesByUser(int posted_by) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM message WHERE posted_by = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, posted_by);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            List<Message> list = new ArrayList<>();
+            while (rs.next()) {
+                Message message = new Message(rs.getInt("message_id"), rs.getInt("posted_by"),
+                rs.getString("message_text"), rs.getLong("time_posted_epoch"));
+                list.add(message);
+                return list;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }   
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -2,10 +2,12 @@ package Service;
 
 import java.util.List;
 
+import DAO.AccountDAO;
 import DAO.MessageDAO;
 import Model.Message;
 
 public class MessageService {
+    AccountDAO accountDAO;
     MessageDAO messageDAO;
     private static final int MAX_AMOUNT_OF_CHARACTERS = 255;
 
@@ -16,14 +18,20 @@ public class MessageService {
         if (message.getMessage_text().length() >= MAX_AMOUNT_OF_CHARACTERS) {
             throw new IllegalArgumentException("Message must be under " + MAX_AMOUNT_OF_CHARACTERS + " characters");
         }
-        if (ifAccountUsernameExists()) {
+        if (accountDAO.ifAccountUsernameExists()) {
             throw new IllegalArgumentException("Username already exists");
         }
         return messageDAO.insertMessage(message);
     }
 
 
+    /**
+     * Service method to obtain all messages in the database.
+     * @return a list of all messages
+     */
     public List<Message> getAllMessages() {
         return messageDAO.getAllMessages();
     }
+
+    private
 }
